e4eb7804b15cd0869a02a0fc91cf1432c3bf659f
Nicson-aroma <aerocrash@gmail.com>
2026-03-30 12:27:37 +0600

    Remove env

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -4,7 +4,7 @@ import emailjs from '@emailjs/browser';
 // Get your PUBLIC KEY from emailjs.com dashboard
 const EMAILJS_PUBLIC_KEY = 'tJjg1g9Fqb8yFklRw';
 const EMAILJS_SERVICE_ID = 'service_iqzzw7z';
-const EMAILJS_TEMPLATE_ID = 'template_mvm8pnp';
+const EMAILJS_TEMPLATE_ID = 'template_0asbruo';
 
 // Initialize EmailJS
 emailjs.init(EMAILJS_PUBLIC_KEY);
